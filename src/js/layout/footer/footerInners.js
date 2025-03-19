d33c31395ce0e64b8c8cc24eb2f62949e6512ef4
zzpillau <zzpillau@gmail.com>
2025-03-19 15:05:00 +0200

    fix: footer links

--- src/js/layout/footer/footerInners.js
+++ src/js/layout/footer/footerInners.js
@@ -18,7 +18,7 @@ const renderFooterInners = () => {
     console.log(social)
     const {name, call, text} = social;
     return createElement('div', ['contact-links', 'action-medium'], {}, '', [
-      createElement('a', ['social-link', `${name}-link`], {href: call}, text, [
+      createElement('a', ['social-link', `${name}-link`], {href: call, target: '_blank'}, text, [
         createElement('img', ['social-icon', `${name}-icon`], {src: `../src/assets/img/${name}.png`})
       ])
   ])
@@ -30,32 +30,6 @@ const renderFooterInners = () => {
   container.append(innerContainer);
 
   return container;
-
-
-
-
-// <div class="contact-links">
-//   <!-- Ссылка для Viber -->
-//   <a href="viber://chat?number=%2B1234567890" class="social-link viber-link">
-//     Написать в Viber
-//   </a>
-
-//   <!-- Ссылка для Telegram -->
-//   <a href="https://t.me/username" class="social-link telegram-link">
-//     Написать в Telegram
-//   </a>
-
-//   <!-- Ссылка для WhatsApp -->
-//   <a href="https://wa.me/1234567890?text=Привет!" class="social-link whatsapp-link">
-//     Написать в WhatsApp
-//   </a>
-
-//   <!-- Ссылка для звонка -->
-//   <a href="tel:+1234567890" class="social-link phone-link">
-//     Позвонить
-//   </a>
-// </div>
-
 
 
 }
